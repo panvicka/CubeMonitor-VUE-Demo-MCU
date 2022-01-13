b9a5367be266a39b1350a794ed9fb18261765968
eliska.veisova <eliska.veisova@ult.de>
2022-01-13 13:25:31 +0100

    .

--- CubeMon-Vue-Demo-STM32F407VGT6U/Core/Inc/lib/di.h
+++ CubeMon-Vue-Demo-STM32F407VGT6U/Core/Inc/lib/di.h
@@ -1,8 +1,27 @@
-/*
- * di.h
+/**
+ * @file di.h
+ * @author panvicka
+ * @date 2.1.2022
+ * @brief digital inputs with falling/rising edge, short press, long press and double press detection
  *
- *  Created on: Jan 8, 2022
- *      Author: panvicka
+ * Both active/low input types can be used. The detection of presses can be deactivated if the inputs is
+ * a toggle switch. For the tactile input types, the double press detection can be deactived as well.
+ *
+ * A support for a direct manipulation of the input state with CUBE MX Monitoring tool is build in. If you
+ * want to use it, define flag ALLOW_CUBEMX_OVERWRITE in you project.
+ *
+ * To use this library define flag LIB_DI in the lib_config.h file and create an enum in init.h file
+ * with the inputs you would like to use.
+ * You then have to initialize each input with \ref input_init function.
+ * @code
+ *	typedef enum {
+ *	MY_INPUT_1,
+ *	MY_INPUT_2,
+ *	MY_INPUT_3,
+ *	DI_NONE
+ *	} digInputs;
+ * @endcode
+ * @note Do not forget to include the DI_NONE at the end of the enum listing.
  */
 
 #ifndef INC_DI_DI_H_
@@ -15,39 +34,104 @@
 #ifdef LIB_DI
 #include <prog/init.h>
 #else
-typedef enum {
+typedef enum digInputs {
 	DI_NONE,
 }digInputs;
 #endif
 
-typedef enum {
-	INPUT_HW_ACTIVE_HIGH, INPUT_HW_ACTIVE_LOW,
+typedef GPIO_PinState (*input_get_hw_state)(digInputs input);
+
+typedef enum digitalInputActions {
+	INPUT_ACT_FALLING_EDGE,
+	INPUT_ACT_RISING_EDGE,
+	INPUT_ACT_SHORT_PRESS,
+	INPUT_ACT_LONG_PRESS,
+	INPUT_ACT_DOUBLE_PRESS,
+	INPUT_ACT_NONE
+} digitalInputActions;
+
+typedef enum digitalInputHWType {
+	/** \brief for inputs that are active if a voltage is applied to the pin (usually with pulldown resistor) */
+	INPUT_HW_ACTIVE_HIGH,
+
+	/** \brief for inputs that are activated when pulled to the GND (always with pullup resistor) */
+	INPUT_HW_ACTIVE_LOW,
 } digitalInputHWType;
 
-typedef enum {
-	INPUT_SW_ON_OFF_INPUT, INPUT_SW_BUTTON,
+typedef enum digitalInputSWType {
+	/** \brief only evaluates logical state and rising/falling edge
+	 * best used for toggle switches or similar type of input */
+	INPUT_SW_ON_OFF_INPUT,
+
+	/** \brief evaluates state and rising/falling edges and also detects three types of button presses
+	 *  best used for tactile switches (for example for navigating in the display menu) */
+	INPUT_SW_BUTTON,
 } digitalInputSWType;
 
 typedef struct digitalInputInitData {
-	uint16_t mcu_pin;
-	GPIO_TypeDef *mcu_port;
+	uint16_t mcu_pin;  ///< MCU GPIO pin number
+	GPIO_TypeDef *mcu_port; ///< MCU GPIO port
 
+	/** \brief minimal time [ms] to consider the input change as a valid change
+	 * recommended value for mechanical switch is 5-10ms
+	 * set to "0" to deactivate this functions */
 	uint16_t debounc_time;
+
+	/** \brief minimal time [ms] to consider the input change to logical "true" as a long press
+	 * set to "0" to deactivate this functions */
 	uint16_t long_press_ms;
+
+	/** \brief maximal time [ms] before two presses that will result in double press,
+	 *  recommended value ~60ms
+	 * 	set to "0" to deactivate this functions */
 	uint16_t double_press_spacing;
-	digitalInputHWType hw_type;
-	digitalInputSWType sw_type;
+
+	digitalInputHWType hw_type; ///< input hw_type
+	digitalInputSWType sw_type; ///< input sw_type
+
+	input_get_hw_state input_get_hw_state; ///< function for getting GPIO state, pass NULL pointer to default to HAL_GPIO_Read
 } digitalInputInitData;
 
-typedef enum {
-	INPUT_DATA_CURRENT_VALUE,
-	INPUT_DATA_DEBOUNCED_VALUE,
-	INPUT_DATA_RISING_EDGE,
-	INPUT_DATA_FALLING_EDGE,
-	INPUT_DATA_DEBOUNC_TIME,
-	INPUT_DATA_SHORT_PRESS,
-	INPUT_DATA_LONG_PRESS,
-	INPUT_DATA_DOUBLE_PRESS,
+typedef enum digitalInputDateTypes {
+	INPUT_DAT_CURR, ///< input current logical value (not debounced), reading directly from HW
+	INPUT_DAT_DEBOUNCED, ///< input debounced logical value
+
+	/** \brief logical rising edge detected
+	 * for INPUT_HW_ACTIVE_HIGH this will be set if the input goes from "0" to "1"
+	 * for INPUT_HW_ACTIVE_LOW this will be set if the input goes from "1" to "0"
+	 * it must be cleaned:
+	 * with /ref input_set function and set value "0"
+	 * or with /ref input_ack_action with argument /ref INPUT_ACT_RISING_EDGE */
+	INPUT_DAT_RISING_EDGE,
+
+	/** \brief logical falling edge detected
+	 * for INPUT_HW_ACTIVE_HIGH this will be set if the input goes from "1" to "0"
+	 * for INPUT_HW_ACTIVE_LOW this will be set if the input goes from "0" to "1"
+	 * it must be cleaned:
+	 * with /ref input_set function and set value "0"
+	 * or with /ref input_ack_action with argument /ref INPUT_ACT_FALLING_EDGE */
+	INPUT_DAT_FALLING_EDGE,
+
+	/** \brief input short press status
+	 * read status with \ref input_get, clean only the short press status with \ref input_set and value "0"
+	 * it must be cleaned:
+	 * with /ref input_set function and set value "0"
+	 * or with /ref input_ack_action with argument /ref INPUT_ACT_SHORT_PRESS */
+	INPUT_DAT_SHORT_PRESS,
+
+	/** \brief input long press status
+	 * read status with \ref input_get, clean only the long press status with \ref input_set and value "0"
+	 * it must be cleaned:
+	 * with /ref input_set function and set value "0"
+	 * or with /ref input_ack_action with argument /ref INPUT_ACT_LONG_PRESS */
+	INPUT_DAT_LONG_PRESS,
+
+	/** \brief input double press status
+	 * read status with \ref input_get, clean only the double press status with \ref input_set and value "0"
+	 * it must be cleaned:
+	 * with /ref input_set function and set value "0"
+	 * or with /ref input_ack_action with argument /ref INPUT_ACT_DOUBLE_PRESS */
+	INPUT_DAT_DOUBLE_PRESS,
 } digitalInputDateTypes;
 
 retStatus input_get(uint32_t input, uint16_t data, int32_t *value);
@@ -56,7 +140,12 @@ retStatus input_set(uint32_t input, uint16_t data, int32_t value);
 retStatus input_init(digInputs input_name,
 		digitalInputInitData digital_input_init_data);
 
-retStatus input_ack_press(digInputs input_name);
+retStatus input_get_init_data(digInputs input_name,
+		digitalInputInitData *digital_input_init_data);
+
+retStatus input_ack_action(digInputs input_name, digitalInputActions action);
+uint8_t input_get_action(digInputs input_name, digitalInputActions action);
+
 uint8_t input_is_short_press(digInputs input_name);
 uint8_t input_is_long_press(digInputs input_name);
 
@@ -65,6 +154,6 @@ dio_states input_state_debounced(digInputs input_name);
 
 void input_handle(void);
 
-__weak GPIO_PinState input_get_hw_state(digInputs input_name);
+__weak GPIO_PinState input_get_hw_HAL(digInputs input_name);
 
 #endif /* INC_DI_DI_H_ */
--- CubeMon-Vue-Demo-STM32F407VGT6U/Core/Src/lib/di.c
+++ CubeMon-Vue-Demo-STM32F407VGT6U/Core/Src/lib/di.c
@@ -1,8 +1,8 @@
-/*
- * di.c
+/**
+ * @file di.c
+ * @author panvicka
+ * @date 2.1.2022
  *
- *  Created on: Jan 8, 2022
- *      Author: panvicka
  */
 
 #include <main.h>
@@ -13,46 +13,51 @@
 //todo move me to def
 #define ALLOW_CUBEMX_OVERWRITE
 
-typedef struct digitalInputDef {
-	uint16_t debounc_counter;
+typedef enum digitalInputSM {
+	INPUT_SM_WAITING_FOR_CHANGE,
+	INPUT_SM_DEBOUNC,
+	INPUT_SM_CONFIRMED,
+	INPUT_SM_BUTTON_PRESS,
+} digitalInputSM;
 
-	uint8_t mx_state;
+typedef struct digitalInputAction {
+	uint8_t detectable;
+	uint8_t detected;
+} digitalInputAction;
+
+typedef struct digitalInputDef {
+
+	uint8_t is_initialized;
+
+	uint8_t mx_logical_state;
 	uint8_t mx_rewrites;
 	uint8_t mx_value;
 
-	uint8_t ris_edge_detectable;
-	uint8_t ris_edge_detected;
-
-	uint8_t fall_edge_detectable;
-	uint8_t fall_edge_detected;
-
-	uint16_t long_press_detected;
-	uint8_t short_press_detected;
-	uint8_t double_press_detected;
-
-	uint8_t change_detected;
-	uint16_t touch_counter;
+	uint8_t double_press_monitoring;
 	uint16_t double_press_counter;
 
-	uint8_t touch_detected;
+	uint16_t counter;
 
 	GPIO_PinState on_state;
 	GPIO_PinState off_state;
 	GPIO_PinState last_confirmed_hw_state;
 
 	digitalInputInitData inits;
+	digitalInputSM state;
+	digitalInputAction actions[INPUT_ACT_NONE];
 
 } digitalInputDef;
 
 static digitalInputDef inputs[DI_NONE - 1];
 
-void static _input_debounc(digInputs input_name);
-dio_states static _input_get_logical_value(digInputs input_name,
+void static _input_debounc(digitalInputDef *input,
+		GPIO_PinState current_HW_state);
+dio_states static _input_get_logical_value(digitalInputDef *input,
 		GPIO_PinState pin_state);
 
 retStatus input_get(uint32_t input_name, uint16_t type, int32_t *value) {
 
-	if (input_name <= DI_NONE) {
+	if (input_name >= DI_NONE && inputs[input_name].is_initialized == 0) {
 		return ENODEV;
 	}
 
@@ -60,27 +65,20 @@ retStatus input_get(uint32_t input_name, uint16_t type, int32_t *value) {
 
 	switch (type) {
 
-	case INPUT_DATA_RISING_EDGE:
-		*value = input->ris_edge_detected;
+	case INPUT_DAT_CURR:
+		*value = (int32_t) _input_get_logical_value(&inputs[input_name],
+				input->inits.input_get_hw_state(input_name));
 		break;
-	case INPUT_DATA_FALLING_EDGE:
-		*value = input->fall_edge_detected;
+	case INPUT_DAT_DEBOUNCED:
+		*value = (int32_t) input->mx_logical_state;
 		break;
-	case INPUT_DATA_CURRENT_VALUE:
-		*value = _input_get_logical_value(input_name,
-				input_get_hw_state(input_name));
-		break;
-	case INPUT_DATA_DEBOUNCED_VALUE:
-		*value = input->mx_state;
-		break;
-	case INPUT_DATA_LONG_PRESS:
-		*value = input->long_press_detected;
-		break;
-	case INPUT_DATA_SHORT_PRESS:
-		*value = input->short_press_detected;
-		break;
-	case INPUT_DATA_DOUBLE_PRESS:
-		*value = input->double_press_detected;
+
+	case INPUT_DAT_RISING_EDGE:
+	case INPUT_DAT_FALLING_EDGE:
+	case INPUT_DAT_LONG_PRESS:
+	case INPUT_DAT_SHORT_PRESS:
+	case INPUT_DAT_DOUBLE_PRESS:
+		*value = (int32_t) input->actions[type].detected;
 		break;
 
 	default:
@@ -94,35 +92,33 @@ retStatus input_get(uint32_t input_name, uint16_t type, int32_t *value) {
 
 retStatus input_set(uint32_t input_name, uint16_t type, int32_t value) {
 
-	if (input_name >= DI_NONE) {
+	if (input_name >= DI_NONE && inputs[input_name].is_initialized == 0) {
 		return ENODEV;
+	}
+
+	// it is not possible to force the input value (INPUT_DAT_CURR or INPUT_DAT_DEBOUNCED) with setter
+	if (type == INPUT_DAT_CURR || type == INPUT_DAT_DEBOUNCED) {
+		return EACCES;
 	}
 
 	digitalInputDef *input = &inputs[input_name];
 
 	switch (type) {
 
-	case INPUT_DATA_RISING_EDGE:
-		if (value == 0) {
-			input->ris_edge_detected = (uint8_t) value;
-		} else {
-			return EOVERFLOW;
-		}
-		break;
-	case INPUT_DATA_FALLING_EDGE:
-		if (value == 0) {
-			input->fall_edge_detected = (uint8_t) value;
-		} else {
-			return EOVERFLOW;
-		}
+	case INPUT_DAT_CURR:
+	case INPUT_DAT_DEBOUNCED:
+		return EACCES;
 		break;
 
-	case INPUT_DATA_DEBOUNC_TIME:
-		if (value < UINT16_MAX) {
-			input->inits.debounc_time = (uint16_t) value;
-		} else {
+	case INPUT_DAT_RISING_EDGE:
+	case INPUT_DAT_FALLING_EDGE:
+	case INPUT_DAT_SHORT_PRESS:
+	case INPUT_DAT_LONG_PRESS:
+	case INPUT_DAT_DOUBLE_PRESS:
+		if (value != 0) {
 			return EOVERFLOW;
 		}
+		input->actions[type].detected = (uint8_t) value;
 		break;
 
 	default:
@@ -131,6 +127,17 @@ retStatus input_set(uint32_t input_name, uint16_t type, int32_t value) {
 	}
 	return EOK;
 
+}
+
+retStatus input_get_init_data(digInputs input_name,
+		digitalInputInitData *digital_input_init_data) {
+	if (input_name >= DI_NONE) {
+		return ENODEV;
+	}
+
+	*digital_input_init_data = inputs[input_name].inits;
+
+	return EOK;
 }
 
 retStatus input_init(digInputs input_name,
@@ -158,70 +165,89 @@ retStatus input_init(digInputs input_name,
 		return EINVAL;
 	}
 
+	if (input->inits.sw_type != INPUT_SW_ON_OFF_INPUT
+			&& input->inits.sw_type != INPUT_SW_BUTTON) {
+		swo_print("di: initialization of wrong SW type");
+		return EINVAL;
+	}
+
+	if (input->inits.input_get_hw_state == NULL) {
+		input->inits.input_get_hw_state = input_get_hw_HAL;
+	}
+
+	if (input->inits.double_press_spacing != 0) {
+		input->actions[INPUT_ACT_DOUBLE_PRESS].detectable = 1;
+	}
+
+	if (input->inits.long_press_ms != 0) {
+		input->actions[INPUT_ACT_LONG_PRESS].detectable = 1;
+	}
+	// short press always detectable
+	input->actions[INPUT_ACT_SHORT_PRESS].detectable = 1;
+
 	/* get the input hw state at time of initialization and set the detectability
 	 * of the rising/falling edge manually once
 	 */
-	input->last_confirmed_hw_state = input_get_hw_state(input_name);
+	input->last_confirmed_hw_state = input->inits.input_get_hw_state(
+			input_name);
 
 	if (input->last_confirmed_hw_state == input->off_state) {
-		input->ris_edge_detectable = 1;
+		input->actions[INPUT_ACT_RISING_EDGE].detectable = 1;
 	} else {
-		input->fall_edge_detectable = 1;
+		input->actions[INPUT_ACT_FALLING_EDGE].detectable = 1;
 	}
+
+	input->is_initialized = 1;
 
 	return EOK;
 
 }
 
 dio_states input_state_debounced(digInputs input_name) {
-	if (input_name < DI_NONE) {
-		return !inputs[input_name].mx_state;
+	if (input_name >= DI_NONE && inputs[input_name].is_initialized == 0) {
+		return !inputs[input_name].mx_logical_state;
 	} else {
 		return ENODEV;
 	}
 }
 
 dio_states input_state_now(digInputs input_name) {
-	if (input_name < DI_NONE) {
-		return input_get_hw_state(input_name);
+	if (input_name >= DI_NONE || inputs[input_name].is_initialized == 0) {
+		return inputs[input_name].inits.input_get_hw_state(input_name);
 	} else {
 		return ENODEV;
 	}
 }
 
 void input_handle(void) {
-	for (uint8_t i = 0; i < DI_NONE; ++i) {
-		_input_debounc(i);
+	for (uint8_t i = 0; i < DI_NONE - 1; i++) {
+		if (inputs[i].is_initialized == 1) {
+			_input_debounc(&inputs[i], inputs[i].inits.input_get_hw_state(i));
+		}
 	}
 }
 
-uint8_t input_is_long_press(digInputs input_name) {
-	if (input_name >= DI_NONE)
+uint8_t input_get_action(digInputs input_name, digitalInputActions action) {
+	if (input_name >= DI_NONE || inputs[input_name].is_initialized == 0) {
 		return ENODEV;
-	return inputs[input_name].long_press_detected;
+	}
+	return inputs[input_name].actions[action].detected;
 }
 
-uint8_t input_is_short_press(digInputs input_name) {
-	if (input_name >= DI_NONE)
+retStatus input_ack_action(digInputs input_name, digitalInputActions action) {
+	if (input_name >= DI_NONE || inputs[input_name].is_initialized == 0) {
 		return ENODEV;
-	return inputs[input_name].short_press_detected;
-}
-
-retStatus input_ack_press(digInputs input_name) {
-
-	if (input_name >= DI_NONE)
-		return ENODEV;
-
-	digitalInputDef *input = &inputs[input_name];
-	input->change_detected = 0;
-	input->short_press_detected = 0;
-	input->long_press_detected = 0;
-	input->touch_counter = 0;
-
+	}
+	inputs[input_name].actions[action].detected = 0;
 	return EOK;
 }
 
-__weak GPIO_PinState input_get_hw_state(digInputs input_name) {
+__weak GPIO_PinState input_get_hw_HAL(digInputs input_name) {
+
+	if (input_name >= DI_NONE) {
+		swo_print("di: invalid input passed to a static function!");
+		return GPIO_PIN_RESET;
+	}
 
 	digitalInputDef *input = &inputs[input_name];
 
@@ -234,26 +260,17 @@ __weak GPIO_PinState input_get_hw_state(digInputs input_name) {
 			return input->off_state;
 		}
 	} else {
-		return HAL_GPIO_ReadPin(inputs[input_name].inits.mcu_port,
-				inputs[input_name].inits.mcu_pin);
+		return HAL_GPIO_ReadPin(input->inits.mcu_port, input->inits.mcu_pin);
 	}
 
 #else
-	return HAL_GPIO_ReadPin(inputs[input_name].inits.mcu_port,
-			inputs[input_name].inits.mcu_pin);
+	return HAL_GPIO_ReadPin(input->inits.mcu_port, input->inits.mcu_pin);
 #endif
 
 }
 
-dio_states static _input_get_logical_value(digInputs input_name,
+dio_states static _input_get_logical_value(digitalInputDef *input,
 		GPIO_PinState pin_state) {
-
-	if (input_name > DI_NONE) {
-		swo_print("di: invalid input passed to a static function");
-		return ENODEV;
-	}
-
-	digitalInputDef *input = &inputs[input_name];
 
 	if (pin_state == GPIO_PIN_RESET) {
 
@@ -269,139 +286,177 @@ dio_states static _input_get_logical_value(digInputs input_name,
 			return 0;
 		}
 	} else {
-		swo_print("di: input in invalid hw state");
+		swo_print("di: input in invalid HW state");
 		return DIO_PROBLEM_OCCURED;
 	}
 }
 
+static void _input_set_action_if_possible(digitalInputAction *action_struct,
+		digitalInputActions action, uint8_t value) {
+
+	if (action_struct[action].detectable == 1 && value == 1) {
+		action_struct[action].detected = 1;
+	} else if (value == 0) {
+		action_struct[action].detected = 0;
+	}
+
+}
+
 void __attribute__((optimize("-O3"))) static _input_debounc(
-		digInputs input_name) {
+		digitalInputDef *input, GPIO_PinState current_HW_state) {
 
-	digitalInputDef *input = &inputs[input_name];
-
-	GPIO_PinState current_HW_state = input_get_hw_state(input_name);
+	digitalInputAction *actions = input->actions;
 
 	/* set detectability of falling/rising edges
 	 * falling edge can be only detectable if the input is in logical "1" state
 	 * rising edge can be only detecble if the input is in logical "0" state
 	 */
 	if (current_HW_state == input->on_state) {
-		input->fall_edge_detectable = 1;
+		actions[INPUT_ACT_FALLING_EDGE].detectable = 1;
 	} else {
-		input->ris_edge_detectable = 1;
+		actions[INPUT_ACT_RISING_EDGE].detectable = 1;
 	}
 
-	// check if there is a change of the input state
-	if (current_HW_state != input->last_confirmed_hw_state) {
+	/*
+	 * increase independent timer for double press monitoring
+	 * if the timer overflows the limit, we can set the single short press active
+	 * to disable double press detection, set the upper limit to "0"
+	 */
+	if (input->double_press_monitoring == 1) {
+		input->double_press_counter =
+				(input->double_press_counter < UINT16_MAX) ?
+						input->double_press_counter + 1 :
+						input->double_press_counter;
 
-		// trigger button press recognition algorithms as well
-		input->touch_detected = 1;
+		if (input->double_press_counter > input->inits.double_press_spacing) {
 
-		// increase debounc counter as long as the change holds but do not overfloat
-		if (input->debounc_counter < UINT16_MAX) {
-			input->debounc_counter++;
+			_input_set_action_if_possible(actions, INPUT_ACT_SHORT_PRESS, 1);
+			input->double_press_monitoring = 0;
+			input->double_press_counter = 0;
 		}
 
-		if (input->debounc_counter >= input->inits.debounc_time) {
-			// the change on the input was a valid input change
-
-			// if the previous state was logical "0" and now there is a confirmed "1", rising edge happened
-			if (input->last_confirmed_hw_state == input->off_state) {
-				if (input->ris_edge_detectable == 1) {
-					input->ris_edge_detected = 1;
-					input->ris_edge_detectable = 0;
-				}
-			} else {
-				// if the previous state was logical "1" and now there is a confirmed "0", falling edge happened
-				if (input->fall_edge_detectable == 1) {
-					input->fall_edge_detected = 1;
-					input->fall_edge_detectable = 0;
-				}
-
-			}
-
-			// set the new state as last confirmed HW state and convert to logical value based on input HW type
-			input->last_confirmed_hw_state = current_HW_state;
-			input->mx_state = _input_get_logical_value(input_name,
-					input->last_confirmed_hw_state);
-
-		}
-
-	} else {
-		// the change was not active long enough, could be a EMI or a mechanical button jitter
-		input->debounc_counter = 0;
 	}
 
-	if (input->inits.sw_type == INPUT_SW_BUTTON) {
+	switch (input->state) {
 
-		if (input->touch_detected == 1) {
-			// change on the input detected, increase the touch counter
+	case INPUT_SM_WAITING_FOR_CHANGE:
+		if (current_HW_state != input->last_confirmed_hw_state) {
+			input->state = INPUT_SM_DEBOUNC;
+			input->counter = 0;
+		}
+		break;
 
-			if (current_HW_state == input->on_state) {
-				if (input->touch_counter < UINT16_MAX) {
-					input->touch_counter++;
-				}
-			}
+	case INPUT_SM_DEBOUNC:
+		input->counter =
+				(input->counter < UINT16_MAX) ?
+						input->counter + 1 : input->counter;
 
-			// if the touch counter is larger then the minimal time for long time press, active it
-			if (input->touch_counter > input->inits.long_press_ms) {
-				input->long_press_detected = 1;
-			}
+		if (input->counter >= input->inits.debounc_time) {
+			input->state = INPUT_SM_CONFIRMED;
+		}
 
+		// the changed input state was not active long enough, probably a jitter
+		if (current_HW_state == input->last_confirmed_hw_state) {
+			input->state = INPUT_SM_WAITING_FOR_CHANGE;
+		}
+		break;
+
+	case INPUT_SM_CONFIRMED:
+		// if the previous state was logical "0" and now there is a confirmed "1", rising edge happened
+		if (input->last_confirmed_hw_state == input->off_state) {
+			_input_set_action_if_possible(actions, INPUT_ACT_RISING_EDGE, 1);
+			actions[INPUT_ACT_RISING_EDGE].detectable = 0;
+
+		} else {
+			// if the previous state was logical "1" and now there is a confirmed "0", falling edge happened
+			_input_set_action_if_possible(actions, INPUT_ACT_FALLING_EDGE, 1);
+			actions[INPUT_ACT_FALLING_EDGE].detectable = 0;
+
+		}
+		// set the new state as last confirmed HW state and convert to logical value based on input HW type
+		input->last_confirmed_hw_state = current_HW_state;
+		input->mx_logical_state = _input_get_logical_value(input,
+				input->last_confirmed_hw_state);
+
+		if (input->inits.sw_type == INPUT_SW_ON_OFF_INPUT) {
+			input->state = INPUT_SM_WAITING_FOR_CHANGE;
+		} else {
 			/*
-			 * if was the button was active longer then the debounc limit and then it was released
-			 * start the timer for double press detection
-			 * if it was released too soon, it may have been only a jitter
+			 * in button mode, we may be expecting a double press
+			 * check if a double press is detected and if so, do not start the button evaluation
+			 * again
 			 */
-			if (current_HW_state == input->off_state) {
-				if (input->touch_counter > input->inits.debounc_time) {
-					input->double_press_counter++;
-				} else {
-					input->touch_detected = 0;
-					input->touch_counter = 0;
-				}
-			}
-
-			// if the input changes to logic "1" again with the counter started, we detected a double press
-			// clean the timers and exit the algoritmus with setting  touch_detected to 0
-			if (current_HW_state == input->on_state
-					&& input->double_press_counter > 0
+			if (input->double_press_monitoring == 1
+					&& current_HW_state == input->on_state
 					&& input->double_press_counter
 							< input->inits.double_press_spacing) {
-				input->double_press_detected = 1;
-				input->touch_detected = 0;
-				input->touch_counter = 0;
+
+				_input_set_action_if_possible(actions, INPUT_ACT_DOUBLE_PRESS,
+						1);
+
+				input->double_press_monitoring = 0;
+				input->counter = 0;
 				input->double_press_counter = 0;
+				input->state = INPUT_SM_WAITING_FOR_CHANGE;
+			} else {
+				input->counter = 0;
+				input->state = INPUT_SM_BUTTON_PRESS;
 			}
 
-			/*
-			 * if the double press counter reaches the maximal value for press spacing,
-			 * we may have detected only a single press
-			 */
-			if (input->double_press_counter
-					> input->inits.double_press_spacing) {
+		}
 
-				if (input->touch_counter > input->inits.debounc_time
-						&& input->touch_counter < input->inits.long_press_ms) {
-					/*
-					 * it the press was longer then the debounc limit but shorter then the long press,
-					 * it was a short press
-					 */
-					input->short_press_detected = 1;
+		break;
 
+	case INPUT_SM_BUTTON_PRESS:
+
+		if (current_HW_state == input->on_state) {
+			input->counter =
+					(input->counter < UINT16_MAX) ?
+							input->counter + 1 : input->counter;
+
+			if (input->counter > input->inits.long_press_ms) {
+				actions[INPUT_ACT_LONG_PRESS].detected = 1;
+			}
+
+		} else if (current_HW_state == input->off_state && input->counter > 0) {
+			if (actions[INPUT_ACT_LONG_PRESS].detected == 0) {
+				input->state = INPUT_SM_WAITING_FOR_CHANGE;
+				input->counter = 0;
+
+				/*
+				 * double press detection is turned off, we can set the short press
+				 * right away
+				 */
+				if (actions[INPUT_ACT_DOUBLE_PRESS].detectable == 0) {
+					input->double_press_monitoring = 0;
+					_input_set_action_if_possible(actions,
+							INPUT_ACT_SHORT_PRESS, 1);
+
+				} else {
+					input->double_press_monitoring = 1;
 				}
 
-				input->double_press_counter = 0;
-				input->touch_detected = 0;
-				input->touch_counter = 0;
+			} else {
+				input->double_press_monitoring = 0;
+
+				_input_set_action_if_possible(actions, INPUT_ACT_LONG_PRESS, 0);
+
+				input->counter = 0;
+				input->state = INPUT_SM_WAITING_FOR_CHANGE;
 			}
 
-			if (input->long_press_detected == 1
-					&& current_HW_state == input->off_state) {
-				input->long_press_detected = 0;
-				input->touch_detected = 0;
-			}
+		} else {
+			input->state = INPUT_SM_WAITING_FOR_CHANGE;
+			input->counter = 0;
 		}
+		break;
+
+	default:
+		input->state = INPUT_SM_WAITING_FOR_CHANGE;
+		swo_print("di: input state machine in invalid state");
+		break;
+
 	}
+
 }
 
